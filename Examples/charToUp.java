f6cd31f4494b18179b18e9e8e5c60499e53673f4
jesuscheng <jesus.cheng@gmail.com>
2014-10-11 10:27:50 -0400

    Added chartoup file to change lower case to upper case

---
+++ Examples/charToUp.java
@@ -0,0 +1,12 @@
+public class charToUp {
+	public static void main(String args[]) {
+		char chLow;
+		char chUp;	
+
+		for(int i=0; i<26; i++) {
+			chLow = (char) ('a' + i);
+			chUp = (char) ('a' - 32 + i); // In ASCII lower case is exactly 32 greater than uppercase
+			System.out.println(chLow + " " + chUp);
+		}
+	}
+}
