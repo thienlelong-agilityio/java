79df050c01cdd5cf331321941db054d9bddaee39
Jesus <jesus.cheng@gmail.com>
2014-10-16 15:13:14 -0400

    Practice inheritance

--- Examples/SuperExample/SuperExample.java
+++ Examples/SuperExample/SuperExample.java
@@ -1,19 +1,25 @@
 class SuperExample {
 	public static void main(String args[]) {
-		SubClass subc1 = new SubClass(1);
-		SubClass subc2 = new SubClass(2);
+		// SubClass subc1 = new SubClass(1);
+		// SubClass subc2 = new SubClass(2);
+		SubClass subc3 = new SubClass(3);
+		System.out.println("SubClass 3: " + subc3.i);    // This is the i from SubClass which was never declared
+		System.out.println("SubClass 3: " + subc3.getI());   // This is the i from SuperClass which was declared
+		SubClass subc4 = new SubClass(4);
 
-		System.out.println("SubClass 1: " + subc1.getI());
-		System.out.println("SubClass 2: " + subc2.getI());
+		// System.out.println("SubClass 1: " + subc1.getI());
+		// System.out.println("SubClass 2: " + subc2.getI());
+		System.out.println("SubClass 3: " + subc3.getI());
+		System.out.println("SubClass 4: " + subc4.getI());
 	}
 }
 
 class SuperClass {
-	private int i;
+	int i;
 
-	SuperClass(int i) {
-		this.i = i;
-	}
+	// SuperClass(int i) {
+	// 	this.i = i;
+	// }
 
 	public int getI() {
 		return this.i;
@@ -21,8 +27,14 @@ class SuperClass {
 }
 
 class SubClass extends SuperClass {
+	int i;
+
+	// SubClass(int p) {
+	// 	super(p);
+	// }
+
 	SubClass(int p) {
-		super(p);
+		super.i = p;
 	}
 }
 
