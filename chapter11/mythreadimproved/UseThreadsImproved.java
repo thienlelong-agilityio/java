a1c7a15d07cd673f10421f55d0136a250ae3e144
Jesus <jesus.cheng@gmail.com>
2014-10-21 17:06:50 -0400

    Added another way to make the thread stop when the child threads stop

--- chapter11/mythreadimproved/UseThreadsImproved.java
+++ chapter11/mythreadimproved/UseThreadsImproved.java
@@ -13,6 +13,7 @@ public class UseThreadsImproved {
 
 		// While you run this part
 		System.out.println("Main Thread Starting. ");
+
 		for(int i=0; i<50; i++) {
 			System.out.print(".");
 			try {
@@ -21,6 +22,34 @@ public class UseThreadsImproved {
 				System.out.println("Main Thread interrupted.");
 			}
 		}
+
+		// // This is another way to stop running this thread after the other child threads stop
+		// // This will run when the child threads are alive using isAlive()
+		// do {
+		// 	System.out.print(".");
+		// 	try {
+		// 		Thread.sleep(100);
+		// 	} catch (InterruptedException e) {
+		// 		System.out.println("Main Thread interrupted.");
+		// 	}
+		// } while (mt1.myThread.isAlive() || mt2.myThread.isAlive() || mt3.myThread.isAlive());
+
+
+		// // Another way of doing it
+		// // join() waits until the thread on which it is called terminates
+		// try {
+		// 	mt1.myThread.join();
+		// 	System.out.println(mt1.myThread.getName() + " joined.");
+
+		// 	mt2.myThread.join();
+		// 	System.out.println(mt2.myThread.getName() + " joined.");
+
+		// 	mt3.myThread.join();
+		// 	System.out.println(mt3.myThread.getName() + " joined.");
+		// } catch (InterruptedException e) {
+		// 	System.out.println("Main Thread interrupted.");
+		// }
+
 		System.out.println("Main Thread ending");
 	}
 }
