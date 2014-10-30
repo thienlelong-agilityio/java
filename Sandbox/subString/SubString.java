26ae6fbddab6c81e077ccdcfb7f24379594e28dc
Jesus <jesus.cheng@gmail.com>
2014-10-30 15:02:54 -0400

    More testing

--- Sandbox/subString/SubString.java
+++ Sandbox/subString/SubString.java
@@ -9,12 +9,15 @@ public class SubString {
 		int n = str.length();
 		System.out.println("Original String: " + str);
 		System.out.println("str.substring(0,0): " + str.substring(0,0));   // Nothing
+		System.out.println("str.substring(0,7): " + str.substring(0,7));  // 0123456
+		System.out.println("str.substring(0,8): " + str.substring(0,8));  // All numbers 01234567
 		System.out.println("str.substring(0,1): " + str.substring(0,1));	// 0
 		System.out.println("str.substring(0,2): " + str.substring(0,2));  // 01
+		System.out.println("str.substring(8,8): " + str.substring(8,8));  // Nothing
 		System.out.println("str.substring(3): " + str.substring(3));  // 3456
 		System.out.println("str.substring(n): " + str.substring(n));  // Nothing
 		System.out.println("str.substring(n-1): " + str.substring(n-1));  // 7, last character
 		System.out.println("str.substring(0): " + str.substring(0));		// 01234567
-
+		// System.out.println("str.substring(9): " + str.substring(9));  // Will not work
 	}
 }
