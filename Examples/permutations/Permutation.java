27f12622786d098afe806b9123158697435c0c1f
jesuscheng <jesus.cheng@gmail.com>
2014-10-30 23:45:12 -0400

    Finished the permutation function

--- Examples/permutations/Permutation.java
+++ Examples/permutations/Permutation.java
@@ -1,30 +1,33 @@
+// Get all the permutations of a string
+// http://stackoverflow.com/questions/4240080/generating-all-permutations-of-a-given-string
+
 public class Permutation {
+
+	public static void permutation(String str) {
+		permutation("", str);
+	}
+
+	private static void permutation(String prefix, String str) {
+		int n = str.length();
+
+		if(n == 0) {
+			System.out.println(prefix);
+		} else {
+			for(int i=0; i<n; i++) {
+				permutation(prefix + str.charAt(i), str.substring(0,i) + str.substring(i+1));
+			}
+		}
+	}
+
+
+	// Main
 	public static void main(String args[]) {
-		String str = "abcd";
+		String str = "abc";
 		System.out.println("Original String: " + str);
 
 		System.out.println("Permutations of String: ");
 
-		System.out.println(permutation(str));
+		// System.out.println(permutation(str));
+		permutation(str);
 	}
-
-	public static String permutation(String str) { 
-		String str1 = "";
-    	permutation(str1, "", str); 
-    	return str1;
-
-	}
-
-	private static void permutation(String str1, String prefix, String str) {
-	    int n = str.length();
-	    if (n == 0) {
-	    	str1 += prefix + " ";
-	    }
-
-	    else {
-	        for (int i = 0; i < n; i++) {
-	            permutation(str1, prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
-	        }
-	    }
-	}	
 }
