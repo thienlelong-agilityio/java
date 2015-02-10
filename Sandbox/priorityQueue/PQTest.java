a07edb35adcdf439c942ff7c7c4d99aaee9bcabd
jesuscheng <jesus.cheng@gmail.com>
2015-02-09 19:16:50 -0500

    Test with priority queue

---
+++ Sandbox/priorityQueue/PQTest.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+
+public class PQTest {
+	public static void main(String args[]) {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		pq.add(7);
+		pq.add(4);
+		pq.add(5);
+		pq.add(3);
+		pq.add(9);
+		pq.add(6);
+
+		System.out.println("Peek: " + pq.peek());
+		System.out.println("Poll (remove min): " + pq.poll());
+	}
+}
